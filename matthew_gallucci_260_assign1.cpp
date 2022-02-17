bacde852a553effdb51d382a0b558c66fc64599e
Matt Gallucci <mattgallucci97@gmail.com>
2022-02-16 16:14:52 -0800

    renamed assign 1 file; assign2 halfway completed

---
+++ matthew_gallucci_260_assign1.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+using namespace std;
+
+const int COLS = 5;
+
+int getTotal(int [][COLS], int);
+double getAverage(int [][COLS], int);
+int getRowTotal(int [][COLS], int);
+int getColumnTotal(int [][COLS], int, int);
+int getHighestInRow(int [][COLS], int);
+int getLowestInRow(int [][COLS], int);
+
+int main()
+{
+	const int ROWS = 4;  
+	
+	
+	int testArray[ROWS][COLS] =
+					{ 
+						{ 15, 95, 59, 67, 3 },
+						{ 33, 61, 41, 26, 13 },
+						{ 27, 5, 69, 88, 24 },
+						{ 22, 34, 99, 29, 68 } 
+					};
+   
+	
+	cout << "The total of the array elements is "
+		 << getTotal(testArray, ROWS)
+		 << endl;
+   
+	cout << "The average value of an element is "
+		 << getAverage(testArray, ROWS)
+		 << endl;
+
+
+	cout << "The total of row 0 is "
+		 << getRowTotal(testArray, 0)
+		 << endl;
+
+	
+	cout << "The total of col 2 is "
+		 << getColumnTotal(testArray, 2, ROWS)
+		 << endl;
+
+    
+	cout << "The highest value in row 2 is "
+		 << getHighestInRow(testArray, 2)
+		 << endl;
+
+	cout << "The lowest value in row 2 is "
+		 << getLowestInRow(testArray, 2)
+		 << endl;
+        
+    system("PAUSE");
+
+	return 0;
+}
+
+
+int getTotal(int array[][COLS], int rows)
+{
+	// initialize the return value
+	int sum = 0;
+	// traverse the array by row
+	for (int i = 0; i < rows; i++) {
+		// iterate through each element in the row and then move on to the next one
+		for (int j = 0; j < COLS; j++) {
+			sum += array[i][j];
+		}
+	}
+	return sum;
+}
+
+
+double getAverage(int array[][COLS], int rows)
+{
+	// get the sum of all values in the array
+	int sum = getTotal(array, rows);
+	// return the total / number of elements
+  return sum / (static_cast<float>(rows) * COLS);
+}
+
+
+int getRowTotal(int array[][COLS], int rowToTotal)
+{
+	// initialize return value
+	int rowTotal = 0;
+	// traverse the specified row and add all the values into sum
+	for (int i = 0; i < COLS; i++) {
+		rowTotal += array[rowToTotal][i];
+	}
+	return rowTotal;
+}
+
+
+int getColumnTotal(int array[][COLS], int colToTotal, int rows)
+{
+	// initialize return value
+	int colTotal = 0;
+	// traverse the specified column and add all the values into sum
+	for (int i = 0; i < rows; i++) {
+		colTotal += array[i][colToTotal];
+	}
+	return colTotal;
+}
+
+
+int getHighestInRow(int array[][COLS], int rowToSearch)
+{
+	// initialize return value to minimum possible integer value
+	int high = INT_MIN;
+	// traverse the specified row and reassign return value when a higher number is found
+	for (int i = 0; i < COLS; i++) {
+		// reassign return value when a higher number is found
+		if (array[rowToSearch][i] > high) {
+			high = array[rowToSearch][i];
+		}
+		// otherwise, keep looking
+	}
+	return high;
+}
+
+
+int getLowestInRow(int array[][COLS], int rowToSearch)
+{
+	// initialize return value to minimum possible integer value
+	int low = INT_MAX;
+	// traverse the specified row
+	for (int i = 0; i < COLS; i++) {
+		// reassign return value when a lower number is found
+		if (array[rowToSearch][i] < low) {
+			low = array[rowToSearch][i];
+		}
+		// otherwise, keep looking
+	}
+	return low;
+}
+
---
+++ matthew_gallucci_260_assign2.cpp
@@ -0,0 +1,110 @@
+// NOTE: The ONLY files that should be #included for this assignment are iostream,  
+// cmath (for the pow function), 
+// and string
+// No other files should be #included
+
+#include <iostream>
+#include <cmath>
+#include <string>
+
+
+// NOTE: The ONLY files that should be #included for this assignment are iostream,  
+// cmath (for the pow function), 
+// and string
+// No other files should be #included
+
+
+using namespace std;
+
+int bin_to_dec(string);
+string dec_to_bin(int);
+int hex_to_dec(string);  
+string dec_to_hex(int);  
+
+
+int main() 
+{
+	
+	cout<<"10000011 binary = "<<bin_to_dec("10000011")<<" decimal\n"; // you should get 131
+	cout<<"01010101 binary = "<<bin_to_dec("01010101")<<" decimal\n"; // you should get 85
+	cout<<"1111111111111111 binary = "<<bin_to_dec("1111111111111111")<<" decimal\n"; // you should get 65,535
+	cout<<"0111111111111111 binary = "<<bin_to_dec("0111111111111111")<<" decimal\n\n"; // you should get 32,767
+	
+	cout<<"255 decimal = "<<dec_to_bin(255)<<" binary\n"; //you should get 11111111
+	cout<<"65532 decimal = "<<dec_to_bin(65532)<<" binary\n"; //you should get 1111111111111100
+	cout<<"12 decimal = "<<dec_to_bin(12)<<" binary\n"; //you should get 1100
+	cout<<"1000000 decimal = "<<dec_to_bin(1000000)<<" binary\n\n"; //you should get 11110100001001000000
+	/*
+	cout<<"ABC hexadecimal = "<<hex_to_dec("ABC")<<" decimal\n"; //you should get 2,748
+	cout<<"F5 hexadecimal = "<<hex_to_dec("F5")<<" decimal\n"; //you should get 245
+	cout<<"1234 hexadecimal = "<<hex_to_dec("1234")<<" decimal\n"; //you should get 4,660
+	cout<<"FDECB hexadecimal = "<<hex_to_dec("FDECB")<<" decimal\n\n"; //you should get 1,040,075
+	
+	cout<<"512 decimal = "<<dec_to_hex(512)<<" hexadecimal\n"; //you should get 200
+	cout<<"5000 decimal = "<<dec_to_hex(5000)<<" hexadecimal\n"; //you should get 1388
+	cout<<"900000 decimal = "<<dec_to_hex(900000)<<" hexadecimal\n"; //you should get DBBA0
+	cout<<"65525 decimal = "<<dec_to_hex(65525)<<" hexadecimal\n\n"; //you should get FFF5
+	*/
+	
+	system("PAUSE");
+	return 0;
+
+}
+
+// Converts any UNsigned binary number to decimal
+
+int bin_to_dec(string s) 
+{
+	// initialize return value
+	int decimal = 0;
+	// index for bit position to traverse the string
+	int bitPosition = s.length() - 1;
+	for (int i = 0; i < s.length(); i++) {
+		if (s[i] == '1') {
+			// add 2^bitPostion to decimal
+			decimal += pow(2, bitPosition);
+		}
+		// decrement bitPosition with every iteration
+		bitPosition--;
+	}
+	return decimal;
+}	
+
+//Converts any non-negative decimal number to binary
+
+string dec_to_bin(int n)  
+{
+	// initialize return value
+	string bin = "";
+	// get the first quotient of the number / 2
+	int quotient = n/2;
+	// set LSB to quotient mod 2
+	int bit = n % 2;
+	bin.insert(0, to_string(bit)); 
+	// continue until the quotient is 0
+	while(quotient != 0) {
+		bit = quotient % 2;
+		bin.insert(0, to_string(bit));
+		quotient = quotient / 2;
+	}
+	return bin;
+}
+
+
+//Converts any UNsigned hexadecimal number to decimal
+
+int hex_to_dec(string s) 
+{
+	return 0;
+}	
+
+
+//Converts any non-negative decimal number to hexadecimalConverts any non-negative decimal number to hexadecimal
+
+string dec_to_hex(int n)  
+{
+	return "not yet implemented";
+}
+
+
+
